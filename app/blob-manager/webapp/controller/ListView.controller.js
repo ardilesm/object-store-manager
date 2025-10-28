96cf3bb16267d53380b54daab8b9f83c7722493e
Ardiles Mozesz <ardiles.mozesz@asml.com>
2025-10-28 09:06:50 +0000

    create listview

--- app/blob-manager/webapp/controller/ListView.controller.js
+++ app/blob-manager/webapp/controller/ListView.controller.js
@@ -5,6 +5,16 @@ sap.ui.define([
 
     return Controller.extend("blobmanager.controller.ListView", {
         onInit() {
+        },
+
+        onPressScanBlob(oEvent){
+            console.log("onPressScanBlob");
+
+            let oButton = oEvent.getSource();
+            let oContext = oButton.getBindingContext();
+            let oRowData = oContext.getObject();
+            
+            sap.m.MessageToast.show("Scan blob for: " + oRowData.id);
         }
     });
 });
